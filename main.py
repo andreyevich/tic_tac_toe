9e5fec01dc92a3dd3e4a24ee0ec4afa6a2270493
Andrei Sirbu <andreisirbuwork1@gmail.com>
2024-12-23 11:22:27 +0200

    Made it work

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/opt/homebrew/bin/python3
+# TODO: add error handling
+# TODO: add gui
 import random
 import minimax
 
@@ -7,6 +9,7 @@ grid = []
 for i in range(1, 10):
     grid.append(str(i))
 
+
 def print_grid():
     print(grid[0] + "|" + grid[1] + "|" + grid[2])
     print("-|-|-")
@@ -14,90 +17,25 @@ def print_grid():
     print("-|-|-")
     print(grid[6] + "|" + grid[7] + "|" + grid[8])
 
-#TODO: implement function so that the computer will always win
-def get_computer_move():
-    available_moves = []
-    for cell in grid:
-        if cell.isnumeric():
-            available_moves.append(cell)
-    return int(random.choice(available_moves)) - 1
-
-# Checks whether the player won in the given rows
-def did_player_win_in_rows(player, rows):
-    win = False
-
-    counter = 0
-    for i in range(len(rows)):
-        if rows[i] == player:
-            counter += 1
-
-        if counter == 3:
-            win = True
-            break
-
-        if (i + 1) % 3 == 0:
-            counter = 0
-            continue
-
-    return win
-
-def did_player_win(player):
-    win = False
-
-    # Did the player win horizontally
-    horizontal_rows = grid
-    if did_player_win_in_rows(player, horizontal_rows):
-        win = True
-
-    # Did the player win vertically
-    vertical_rows = []
-    for i in range(0, 3):
-        vertical_rows.append(grid[i])
-        vertical_rows.append(grid[i + 3])
-        vertical_rows.append(grid[i + 6])
-    if did_player_win_in_rows(player, vertical_rows):
-        win = True
-
-    # Did the player win diagonally, baby!
-    if grid[0] == grid[4] == grid[8] == player or grid[2] == grid[4] == grid[6] == player:
-        win = True
-
-    if win:
-        print(player, "won!")
-    return win
-
-def is_grid_full():
-    for cell in grid:
-        if cell != "X" and cell != "O":
-            return False
-    return True
 
 while True:
     print_grid()
 
-    human_move = int(input("Human: enter your move: "))
-
-    # TODO: don't allow the user to be stupid
-    # # In case the user doesn't enter a number
-    # try:
-    #     human_move = int(input("Human: enter your move: "))
-    # except:
-    #     print("Invalid move... Please try again.")
-    #     continue
-
-    # In case the number is invalid.
-    if grid[human_move - 1] == "O" or grid[human_move - 1] == "X" or human_move < 1 or human_move > len(grid):
-        print("Invalid move... Please try again.")
-        break
-
+    human_move = int(input("Human, enter your move: "))
     grid[human_move - 1] = "X"
-    if did_player_win("X"):
+    if minimax.is_terminal(grid) == -1:
+        print("Human, you won!")
         break
-
-    if is_grid_full():
+    elif minimax.is_terminal(grid) == 0:
+        print("Tie!")
         break
 
     computer_move = minimax.best_move(grid)
     grid[computer_move] = "O"
-    print("The computer moved...")
-    running = not did_player_win("O")
+
+    if minimax.is_terminal(grid) == 1:
+        print("Human, you're a loser!")
+        break
+    elif minimax.is_terminal(grid) == 0:
+        print("Tie!")
+        break
--- minimax.py
+++ minimax.py
@@ -1,113 +1,68 @@
-"""
-X - min player
-O - max player (computer)
-
-Implementation:
-
-s0 - initial state
-player(s) - returns which player to move in state s
-actions(s) - returns legal moves in state s
-result(s, a) - returns the state after action a taken in state s
-terminal(s) - checks if the state s is a terminal state
-"""
 import math
-# import sys
 
+# The computer is the max player
 MAX_PLAYER = "O"
 MIN_PLAYER = "X"
 
-# def player(state):
-#     max_player_count = 0
-#     min_player_count = 0
-#     for cell in state:
-#         if cell == MAX_PLAYER:
-#             max_player_count += 1
-#         elif cell == MIN_PLAYER:
-#             min_player_count += 1
 
-#     if max_player_count <= min_player_count:
-#         return MAX_PLAYER
-#     else:
-#         return MIN_PLAYER
-
-# def actions(state):
-#     result = []
-#     for cell in state:
-#         if cell != MAX_PLAYER and cell != MIN_PLAYER:
-#             result.append(state.index(cell))
-#     return result
-
-# def result(state=list, action=int, player=str):
-#     state[action] = player
-#     return state
-
-def is_terminal(state):
-    full = True
-    for cell in state:
+def is_terminal(grid):
+    is_grid_full = True
+    for cell in grid:
         if cell != MAX_PLAYER and cell != MIN_PLAYER:
-            full = False
+            is_grid_full = False
             break
-    if full:
+    if is_grid_full:
         return 0
-    # TODO: unhardcode??
+
     winning_positions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], 
                          [0, 3, 6], [1, 4, 7], [2, 5, 8], 
                          [0, 4, 8], [2, 4, 6]]
     for winning_position in winning_positions:
-        valid = True
-        for cell_index in winning_position:
-            if state[cell_index] != MAX_PLAYER or state[cell_index] != MIN_PLAYER:
-                valid = False
+        is_winning_postion_valid = True
+        for i in winning_position:
+            if grid[i] != MAX_PLAYER and grid[i] != MIN_PLAYER:
+                is_winning_postion_valid = False
                 break
-        if valid:
-            if state[winning_position[0]] == state[winning_position[1]] == state[winning_position[2]]:
-                winning_player = state[winning_position[0]]
+        if is_winning_postion_valid:
+            if grid[winning_position[0]] == grid[winning_position[1]] == grid[winning_position[2]]:
+                winner = grid[winning_position[0]]
                 
-                if winning_player == MAX_PLAYER:
+                if winner == MAX_PLAYER:
                     return 1
                 return -1
 
-# def max_value(state):
-#     is_terminal, utility = is_terminal(state)
-#     if is_terminal:
-#         return utility
-#     state_value = -sys.maxsize # the interpreter's word size (i.e. the max length of strings, lists, etc.)
-#     for action in actions(state):
-#         state_value = max(state_value, min_value(result(state, action, MAX_PLAYER)))
-#     return state_value
+def minimax(grid, is_maximising):
+    score = is_terminal(grid)
+    if score:
+        return score
 
-# def min_value(state):
-#     is_terminal, utility = is_terminal(state)
-#     if is_terminal:
-#         return utility
-#     state_value = sys.maxsize # the interpreter's word size (i.e. the max length of strings, lists, etc.)
-#     for action in actions(state):
-#         state_value = min(state_value, max_value(result(state, action, MIN_PLAYER)))
-#     return state_value
-
-def minimax(grid):
-    result = is_terminal(grid)
-    if result:
-        return result
-
-    best_score = -math.inf
-    for i in range(len(grid)):
-        if grid[i] != MIN_PLAYER and grid[i] != MAX_PLAYER:
-            grid[i] = MAX_PLAYER
-            score = minimax(grid)
-            grid[i] = str(i + 1)
-            best_score = max(score, best_score)
-
-    return best_score 
+    if is_maximising:
+        best_score = -math.inf
+        for i in range(len(grid)):
+            if grid[i] != MIN_PLAYER and grid[i] != MAX_PLAYER:
+                grid[i] = MAX_PLAYER
+                score = minimax(grid, False)
+                grid[i] = str(i + 1)
+                best_score = max(score, best_score)
+        return best_score
+    else:
+        best_score = math.inf
+        for i in range(len(grid)):
+            if grid[i] != MIN_PLAYER and grid[i] != MAX_PLAYER:
+                grid[i] = MIN_PLAYER
+                score = minimax(grid, True)
+                grid[i] = str(i + 1)
+                best_score = min(score, best_score)
+        return best_score 
 
 def best_move(grid):
     best_score = -math.inf
-    move = 0
+    move = None
 
     for i in range(len(grid)):
         if grid[i] != MIN_PLAYER and grid[i] != MAX_PLAYER:
             grid[i] = MAX_PLAYER
-            score = minimax(grid)
+            score = minimax(grid, False)
             grid[i] = str(i + 1)
 
             if score > best_score:
